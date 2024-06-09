5ef771df2049050a93e87e0e8e5a0e4a9074b3e7
adam.beseler <adam.beseler@gmail.com>
2024-06-09 12:18:46 -0500

    added quicker retry to apphost db migration apply

--- source/Beseler.AppHost/Program.cs
+++ source/Beseler.AppHost/Program.cs
@@ -12,6 +12,8 @@ builder.AddContainer("dbdeploy", "abeseler/dbdeploy")
     .WithEnvironment("Deploy__StartingFile", "migrations.json")
     .WithEnvironment("Deploy__DatabaseProvider", "postgres")
     .WithEnvironment("Deploy__ConnectionString", $"Host=host.docker.internal;Port=15432;Database=bslr;Username={username.Resource.Value};Password={password.Resource.Value}")
+    .WithEnvironment("Deploy__ConnectionAttempts", "10")
+    .WithEnvironment("Deploy__ConnectionRetryDelaySeconds", "1")
     .WithEnvironment("Serilog__MinimumLevel__Default", "Debug")
     .WithBindMount("../../data", "/app/Migrations");
 
